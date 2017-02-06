b5c70a5ad5db69dfbf7bdeb3f8e7b24571fae020
Unknown <nguyenquangsang2810@gmail.com>
2017-02-06 16:56:26 +0700

    Try out

---
+++ thinkster/backend/flapper-news/models/Comments.js
@@ -0,0 +1,13 @@
+/**
+ * Created by NguyenQuangSang on 2/2/2017.
+ */
+var mongoose = require('mongoose');
+
+var CommentSchema = new mongoose.Schema({
+    body: String,
+    author: String,
+    upvotes: {type: Number, default: 0},
+    post: { type: mongoose.Schema.Types.ObjectId, ref: 'Post' }
+});
+
+mongoose.model('Comment', CommentSchema);
---
+++ thinkster/backend/flapper-news/models/Posts.js
@@ -0,0 +1,17 @@
+/**
+ * Created by NguyenQuangSang on 2/2/2017.
+ */
+var mongoose = require('mongoose');
+
+var PostSchema = new mongoose.Schema({
+    title: String,
+    link: String,
+    upvotes: {type: Number, default: 0},
+    comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }]
+});
+PostSchema.methods.upvote = function(cb) {
+    this.upvotes += 1;
+    this.save(cb);
+};
+
+mongoose.model('Post', PostSchema);
